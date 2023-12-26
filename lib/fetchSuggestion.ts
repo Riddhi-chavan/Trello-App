4144b0c9a57aa725a6c687dbf18948e6720bcfc7
Riddhi-chavan <riddhic164@gmail.com>
2023-12-27 01:25:59 +0530

    "updated fetchSuggestion"

--- lib/fetchSuggestion.ts
+++ lib/fetchSuggestion.ts
@@ -3,7 +3,12 @@ import formatTodosForAI  from  "./formatTodosForAI"
 const fetchSuggestion = async (board : Board ) => {
     const todos = formatTodosForAI(board);
 
-    const res = await fetch("http://localhost:3000/api/generateSummery" , {
+    const baseUrl = process.env.NODE_ENV === 'production'
+    ? 'https://your-vercel-app.vercel.app'
+    : 'http://localhost:3000';
+    
+
+    const res = await fetch(`${baseUrl}/api/generateSummery` , {
         method: "POST",
         headers : {
             "Content-Type" : "application/json",
